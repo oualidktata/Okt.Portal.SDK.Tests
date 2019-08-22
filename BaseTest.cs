fd7840eb39235877efa265a377a65229bddcdd78
Oualid Ktata <oualid.ktata@gmail.com>
2019-08-22 20:12:45 +0000

    Update BaseTest.cs

--- BaseTest.cs
+++ BaseTest.cs
@@ -25,15 +25,15 @@ namespace Portal.SDK.Test
         {
              AssetteApiSettings _assetteApiSettings = new AssetteApiSettings
                 {
-                    AuthUrl = @"https://app.stg.assette.com/services/id/auth/token",
-                    BaseAddress = @"https://app.stg.assette.com/app/admin",
+                    AuthUrl = @"https://app.SECRET.com/services/id/auth/token",
+                    BaseAddress = @"https://app.SECRET.com/app/admin",
                     Credentials = new AssetteApiCredentials
                     {
                         ClientId = "ADDN",
-                        ClientSecret = "3D5AC9A019200A5A4D0F0D8A38C8CAB0A8BFF5DC894FBCE0EE18B9B138937A3BF1A9AEAEC8E779A8384990B4D5DD2C7FA5F89D0E5FBB9DFDA1311BDA73A62EE3",
-                        UserName = @"ADDN\assetteuser",
-                        Password = "assette2client"
-                        //https://app.stg.assette.com/app/admin/swagger/index.html pour swagger
+                        ClientSecret = "SECRET",
+                        UserName = @"ADDN\SECRET",
+                        Password = "SECRET"
+                        //https://app.SECRET.assette.com/app/admin/swagger/index.html pour swagger
                     }
              };
                 //_client = new AssetteApiClient(_assetteApiSettings) as IClient;
@@ -43,12 +43,12 @@ namespace Portal.SDK.Test
         private async Task<IClient> InitAddendaSDKClient()
         {    var authSettings = new OktaSettings()
                 {
-                    ClientId = "0oauaatqnDFhZFgkp356",
-                    ClientSecret = "wHWb6jbjEXP21Y0pJ6JvuYejBDmLL13CE4qHSkV8",
-                    TokenUrl = "https://dev-193568.okta.com/oauth2/default/v1/token"
+                    ClientId = "SECRET",
+                    ClientSecret = "SECRET",
+                    TokenUrl = "https://dev-SECRET.okta.com/oauth2/default/v1/token"
                 };
                 var manager = new PortalClientSDKManager();
-                //var aToken = "eyJraWQiOiJINlZwVVA1YmFXQzdNcFRFX2VycmFBZ2ZmRnliRzNyQTZlS2NQbVhYc1dzIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULlliM1hQY2pRNVlnS2J4aUZqZTVfZUloZUNqYjJVRk9wSVBiTWJKS3JBZGMiLCJpc3MiOiJodHRwczovL2Rldi0xOTM1Njgub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTY0NTExNTM3LCJleHAiOjE1NjQ1MTUxMzcsImNpZCI6IjBvYXVhYXRxbkRGaFpGZ2twMzU2Iiwic2NwIjpbImN1c3RvbV9zY29wZSJdLCJzdWIiOiIwb2F1YWF0cW5ERmhaRmdrcDM1NiJ9.Frs9wv4dlKm89iIDzGGleoMmYcpSplLgPCf70cRUtW1EPB34Why2TEPyFeYpZpTP_rzY8zc9OFVWHLObwX3DsoakHeqaHwAQCmpDapPfGAnsOKMHHe9b9c6KOiELWGwQJ6q0jpM7FYpEHQuDbusvwTuzfhohwK-CeQP4suwREitrvy0GAaGEHslXguIG7ihczEgh2s0k39kkb7vRma3Gzec9vJ2Jx7syX5XpRn-rTU3OLkr6tSYHvxqhfs0HiYCa39GxodGi8xVHj8GfdPN8HJXDWHV0vMGz0m5g0znSkyVH5mWyk-mmy4Z4bSabes-mcqTjfqSR99t_Zn0lqnB_VA";
+                //var aToken = "SECRET.eyJ2ZXIiOjEsImp0aSI6IkFULlliM1hQY2pRNVlnS2J4aUZqZTVfZUloZUNqYjJVRk9wSVBiTWJKS3JBZGMiLCJpc3MiOiJodHRwczovL2Rldi0xOTM1Njgub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTY0NTExNTM3LCJleHAiOjE1NjQ1MTUxMzcsImNpZCI6IjBvYXVhYXRxbkRGaFpGZ2twMzU2Iiwic2NwIjpbImN1c3RvbV9zY29wZSJdLCJzdWIiOiIwb2F1YWF0cW5ERmhaRmdrcDM1NiJ9.Frs9wv4dlKm89iIDzGGleoMmYcpSplLgPCf70cRUtW1EPB34Why2TEPyFeYpZpTP_rzY8zc9OFVWHLObwX3DsoakHeqaHwAQCmpDapPfGAnsOKMHHe9b9c6KOiELWGwQJ6q0jpM7FYpEHQuDbusvwTuzfhohwK-CeQP4suwREitrvy0GAaGEHslXguIG7ihczEgh2s0k39kkb7vRma3Gzec9vJ2Jx7syX5XpRn-rTU3OLkr6tSYHvxqhfs0HiYCa39GxodGi8xVHj8GfdPN8HJXDWHV0vMGz0m5g0znSkyVH5mWyk-mmy4Z4bSabes-mcqTjfqSR99t_Zn0lqnB_VA";
                 return await manager.GetAddendaClient(authSettings, "https://localhost:44324/");//Uncomment for Addenda API
            
 
